cef858325f4c8492efaf3e0955dc4ea906d22ae2
Phyo Lay <juniorphyolay10@gmail.com>
2024-02-02 01:40:06 +0630

    first

---
+++ src/App.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h3 class="text-decoration-underline mb-3">Vue-Todo-App</h3>
+
+        <div class="p-4 rounded mb-5 shadow-lg">
+          <input
+            type="text"
+            @keyup.enter="addList"
+            v-model="inputTodo"
+            placeholder="Add to do list"
+            class="form-control shadow border-0"
+            :class="{ 'is-invalid': inputInvalid }"
+          />
+          <div v-if="inputInvalid" class="invalid-feedback">
+            You need to fill this field.
+          </div>
+          <button class="btn btn-sm btn-primary mt-4" @click="addList">
+            {{ editingTodo ? "Update" : "Add" }}
+          </button>
+        </div>
+
+        <h5 class="mb-3">Todo Lists ~</h5>
+        <ul class="list-group">
+          <li
+            class="list-group-item shadow-sm d-flex justify-content-between pt-3 mb-3"
+            v-for="todo in todos"
+            :key="todo.id"
+            @dblclick="doneTodo(todo)"
+          >
+            <p>
+              <b>~ </b>
+              <span style="cursor: pointer" title="double click">
+                <i> {{ todo.name }}</i></span
+              >
+            </p>
+            <div>
+              <button
+                title="edit"
+                class="btn btn-sm btn-outline-info"
+                @click="editTodo(todo)"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <button
+                title="done"
+                class="btn btn-sm btn-outline-primary mx-2"
+                @click="doneTodo(todo)"
+              >
+                <i class="bi bi-check-square"></i>
+              </button>
+              <button
+                title="delete"
+                class="btn btn-sm btn-outline-danger"
+                @click="deleteTodo(todo.id)"
+              >
+                <i class="bi bi-trash"></i>
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-6 ps-5 mt-2">
+        <h5 class="mb-4">Done todo lists ~</h5>
+        <ul class="list-group">
+          <li
+            class="list-group-item shadow-sm d-flex justify-content-between pt-3 mb-3"
+            v-for="doneTodo in doneTodos"
+            :key="doneTodo.id"
+            @dblclick="undoneTodo(doneTodo)"
+          >
+            <p>
+              <b>~ </b>
+              <span style="cursor: pointer" title="double click">
+                <i> {{ doneTodo.name }}</i></span
+              >
+            </p>
+            <div>
+              <button
+                title="undone"
+                class="btn btn-sm btn-outline-primary mx-2"
+                @click="undoneTodo(doneTodo)"
+              >
+                <i class="bi bi-arrow-counterclockwise"></i>
+              </button>
+              <button
+                title="delete"
+                class="btn btn-sm btn-outline-danger"
+                @click="deleteDoneTodo(doneTodo)"
+              >
+                <i class="bi bi-trash"></i>
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const Id = Math.floor(Math.random() * 10) + 1;
+const inputTodo = ref("");
+const editingTodo = ref();
+const doneTodos = ref([]);
+const todos = ref([
+  {
+    id: Id,
+    name: "wake up early",
+  },
+]);
+
+const inputInvalid = ref(false);
+
+// create
+const addList = () => {
+  if (inputTodo.value.trim() !== "") {
+    // update
+    if (editingTodo.value) {
+      editingTodo.value.name = inputTodo.value;
+      editingTodo.value = "";
+      inputTodo.value = "";
+    } else {
+      const newTodo = {
+        id: Math.floor(Math.random() * 10),
+        name: inputTodo.value,
+        done: false,
+      };
+      todos.value.push(newTodo);
+      inputTodo.value = "";
+    }
+  } else {
+    inputInvalid.value = true;
+  }
+};
+
+// edit
+const editTodo = (todo) => {
+  editingTodo.value = todo;
+  inputTodo.value = todo.name;
+};
+
+// delete
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+
+// done
+const doneTodo = (todo) => {
+  todo.done = true;
+  doneTodos.value.push(todo);
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+// undone
+const undoneTodo = (doneTodo) => {
+  doneTodo.done = false;
+  todos.value.push(doneTodo);
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+// delete doneTodo
+const deleteDoneTodo = (doneTodo) => {
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+</script>
+
+<style scoped></style>
---
+++ src/Test.vue
@@ -0,0 +1,310 @@
+<!-- <template>
+  <div class="container p-5">
+    <h4>My Todo Lists</h4>
+    <div class="input-group w-50">
+      <input type="text" v-model="inputTodo" class="form-control" />
+      <button class="btn btn-primary" @click="addList">
+        {{ editingTodo ? "Update" : "Add" }}
+      </button>
+    </div>
+
+    <h5 class="mt-3">Todo List</h5>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        class="mt-3"
+        @dblclick="markAsDone(todo)"
+        title="double click"
+      >
+        <span style="cursor: pointer">{{ todo.name }}</span>
+        <button class="btn btn-sm btn-success mx-2" @click="markAsDone(todo)">
+          Done
+        </button>
+        <button class="btn btn-sm btn-warning mx-2" @click="editTodo(todo)">
+          Edit
+        </button>
+        <button class="btn btn-sm btn-danger" @click="deleteTodo(todo)">
+          Delete
+        </button>
+      </li>
+    </ul>
+
+    <h5 class="mt-5">Done List</h5>
+    <ul>
+      <li
+        v-for="doneTodo in doneTodos"
+        :key="doneTodo.id"
+        @dblclick="markAsUndone(doneTodo)"
+      >
+        <span style="cursor: pointer">{{ doneTodo.name }}</span>
+        <button
+          class="btn btn-sm btn-warning border mx-2"
+          @click="markAsUndone(doneTodo)"
+        >
+          Undone
+        </button>
+        <button
+          class="btn btn-sm btn-danger border"
+          @click="deleteDone(doneTodo)"
+        >
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const Id = Math.floor(Math.random() * 10) + 1;
+const todos = ref([]);
+const doneTodos = ref([]);
+const inputTodo = ref("");
+const editingTodo = ref(null);
+
+const addList = () => {
+  if (inputTodo.value !== "") {
+    if (editingTodo.value) {
+      // Update existing todo
+      editingTodo.value.name = inputTodo.value;
+      editingTodo.value = null;
+    } else {
+      // Add new todo
+      const newTodo = {
+        id: Id,
+        name: inputTodo.value,
+        done: false,
+      };
+      todos.value.push(newTodo);
+    }
+    inputTodo.value = "";
+  }
+};
+
+const markAsDone = (todo) => {
+  todo.done = true;
+  doneTodos.value.push(todo);
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const markAsUndone = (doneTodo) => {
+  doneTodo.done = false;
+  todos.value.push(doneTodo);
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+const deleteTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const deleteDone = (doneTodo) => {
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+const editTodo = (todo) => {
+  editingTodo.value = todo;
+  inputTodo.value = todo.name;
+};
+</script>
+
+<style scoped></style> -->
+
+<!-- <template>
+  <div class="container p-5">
+    <h4>My Todo Lists</h4>
+    <div class="input-group w-50">
+      <input type="text" v-model="inputTodo" class="form-control" />
+      <button class="btn btn-primary" @click="addList">Add</button>
+    </div>
+
+    <h5 class="mt-3">Todo List</h5>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.name }}
+        <button class="btn btn-sm btn-success mx-2" @click="markAsDone(todo)">
+          Done
+        </button>
+        <button class="btn btn-sm btn-danger" @click="deleteTodo(todo)">
+          Delete
+        </button>
+      </li>
+    </ul>
+
+    <h5 class="mt-5">Done List</h5>
+    <ul>
+      <li v-for="doneTodo in doneTodos" :key="doneTodo.id">
+        {{ doneTodo.name }}
+        <button
+          class="btn btn-sm btn-warning border mx-2"
+          @click="markAsUndone(doneTodo)"
+        >
+          Undone
+        </button>
+        <button
+          class="btn btn-sm btn-danger border"
+          @click="deleteDone(doneTodo)"
+        >
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const Id = Math.floor(Math.random() * 10) + 1;
+const todos = ref([]);
+const doneTodos = ref([]);
+const inputTodo = ref("");
+
+const addList = () => {
+  if (inputTodo.value !== "") {
+    const newTodo = {
+      id: Id,
+      name: inputTodo.value,
+      done: false,
+    };
+    todos.value.push(newTodo);
+    inputTodo.value = "";
+  }
+};
+
+const markAsDone = (todo) => {
+  todo.done = true;
+  doneTodos.value.push(todo);
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const markAsUndone = (doneTodo) => {
+  doneTodo.done = false;
+  todos.value.push(doneTodo);
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+const deleteTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const deleteDone = (doneTodo) => {
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+</script>
+
+<style scoped></style> -->
+
+<!-- <template>
+  <div class="container p-5">
+    <h4>My Todo Lists</h4>
+    <div class="input-group w-50">
+      <input type="text" v-model="inputTodo" class="form-control" />
+      <button class="btn btn-primary" @click="addList">
+        {{ editingTodo ? "Update" : "Add" }}
+      </button>
+    </div>
+
+    <h5 class="mt-3">Todo List</h5>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        class="mt-3"
+        @dblclick="markAsDone(todo)"
+        title="double click"
+      >
+        <span style="cursor: pointer">{{ todo.name }}</span>
+        <button class="btn btn-sm btn-success mx-2" @click="markAsDone(todo)">
+          Done
+        </button>
+        <button class="btn btn-sm btn-warning mx-2" @click="editTodo(todo)">
+          Edit
+        </button>
+        <button class="btn btn-sm btn-danger" @click="deleteTodo(todo)">
+          Delete
+        </button>
+      </li>
+    </ul>
+
+    <h5 class="mt-5">Done List</h5>
+    <ul>
+      <li
+        v-for="doneTodo in doneTodos"
+        :key="doneTodo.id"
+        @dblclick="markAsUndone(doneTodo)"
+      >
+        <span style="cursor: pointer">{{ doneTodo.name }}</span>
+        <button
+          class="btn btn-sm btn-warning border mx-2"
+          @click="markAsUndone(doneTodo)"
+        >
+          Undone
+        </button>
+        <button
+          class="btn btn-sm btn-danger border"
+          @click="deleteDone(doneTodo)"
+        >
+          Delete
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const Id = Math.floor(Math.random() * 10) + 1;
+const todos = ref([]);
+const doneTodos = ref([]);
+const inputTodo = ref("");
+const editingTodo = ref(null);
+
+const addList = () => {
+  if (inputTodo.value !== "") {
+    if (editingTodo.value) {
+      // Update existing todo
+      editingTodo.value.name = inputTodo.value;
+      editingTodo.value = null;
+    } else {
+      // Add new todo
+      const newTodo = {
+        id: Id,
+        name: inputTodo.value,
+        done: false,
+      };
+      todos.value.push(newTodo);
+    }
+    inputTodo.value = "";
+  }
+};
+
+const markAsDone = (todo) => {
+  todo.done = true;
+  doneTodos.value.push(todo);
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const markAsUndone = (doneTodo) => {
+  doneTodo.done = false;
+  todos.value.push(doneTodo);
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+const deleteTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const deleteDone = (doneTodo) => {
+  doneTodos.value = doneTodos.value.filter((t) => t !== doneTodo);
+};
+
+const editTodo = (todo) => {
+  editingTodo.value = todo;
+  inputTodo.value = todo.name;
+};
+</script>
+
+<style scoped></style> -->
